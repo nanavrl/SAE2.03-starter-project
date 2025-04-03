3d1fdcfb60c89e27d349234763b7085b6c85ea55
Anna Viroulaud <anna.viroulaud@etu.unilim.fr>
2025-04-03 15:28:29 +0200

    maj back et front

--- admin/component/MovieForm/script.js
+++ admin/component/MovieForm/script.js
@@ -1,44 +1,15 @@
 
-
-let templateFile = await fetch('./component/Log/template.html');
+let templateFile = await fetch('./component/MovieForm/template.html');
 let template = await templateFile.text();
 
-let templateLi = "<li>[{{time}}] {{txt}}</li>";
-let templateLiLast = "<li class='last'>[{{time}}] {{txt}}<span class='clignotant'> #</span></li>";
 
-let Log = {};
+let MovieForm = {};
 
-let history = []; // non exporté, interne au module du composant (encapsulation)
-
-let add = function(txt){ // non exporté, interne au module du composant (encapsulation)
-    let d = new Date();
-    let h = d.getHours();
-    let m = d.getMinutes();
-    let s = d.getSeconds();
-    let time = h + ':' + m + ':' + s;
-    let log = {time: time, txt: txt};
-    history.push(log);
-}
-
-let formatHistory= function(){ // non exporté, interne au module du composant (encapsulation)
-    let html = "";
-    if (history.length == 0) return html;
-    for (let i=0; i<history.length-1; i++){ 
-        let log = history[i];
-        html += templateLi.replace('{{time}}', log.time).replace('{{txt}}', log.txt);
-    }
-    let lastLog = history[history.length-1];
-    html += templateLiLast.replace('{{time}}', lastLog.time).replace('{{txt}}', lastLog.txt); 
-    return html;
-}
-
-Log.format = function(txt){
-    add(txt);
+MovieForm.format = function(handler){
     let html= template;
-    html = html.replace("{{logs}}", formatHistory());
+    html = html.replace('{{handler}}', handler);
     return html;
 }
 
 
-export {Log};
-
+export {MovieForm};
---
+++ admin/data/dataMovie.js
@@ -0,0 +1,38 @@
+// URL où se trouve le répertoire "server" sur mmi.unilim.fr
+let HOST_URL = "https://mmi.unilim.fr/~lippler1/SAE203-Manon";
+
+let DataMovie = {};
+
+DataMovie.requestMovies = async function () {
+  let answer = await fetch(HOST_URL + "/server/script.php?todo=getMovie");
+  let movies = await answer.json();
+  return movies;
+};
+
+/** DataMovie.add
+ *
+ * Prend en paramètre un objet FormData (données de formulaire) à envoyer au serveur.
+ * Ces données sont incluses dans une requête HTTP en méthode POST.
+ * Une requête POST au lieu de GET n'affiche pas les données dans l'URL (plus discret).
+ * Les données sont placées dans le corps (body) de la requête HTTP. Elles restent visibles mais
+ * en utilisant les outils de développement du navigateur (Network > Payload).
+ * La requête comprend aussi un paramètre todo valant add pour indiquer au serveur qu'il
+ * s'agit d'une mise à jour (car on a codé le serveur pour qu'il sache quoi faire en fonction de la valeur de todo).
+ *
+ * @param {*} fdata un objet FormData contenant les données du formulaire à envoyer au serveur.
+ * @returns la réponse du serveur.
+ */
+DataMovie.addMovie = async function (fdata) {
+  // fetch possède un deuxième paramètre (optionnel) qui est un objet de configuration de la requête HTTP:
+  //  - method : la méthode HTTP à utiliser (GET, POST...)
+  //  - body : les données à envoyer au serveur (sous forme d'objet FormData ou bien d'une chaîne de caractères, par exempe JSON)
+  let config = {
+    method: "POST", // méthode HTTP à utiliser
+    body: fdata, // données à envoyer sous forme d'objet FormData
+  };
+  let answer = await fetch(HOST_URL + "/server/script.php?todo=addMovie", config);
+  let data = await answer.json();
+  return data;
+};
+
+export { DataMovie };
