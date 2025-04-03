58f33931c099e68b35ea1ccfb2887239a415ce1a
Anna Viroulaud <anna.viroulaud@etu.unilim.fr>
2025-04-03 10:50:19 +0200

    maj credentials bdd

--- server/model.php
+++ server/model.php
@@ -14,6 +14,6 @@
  * DBPWD : Mot de passe pour se connecter à la base de données.
  */
 define("HOST", "localhost");
-define("DBNAME", "mora");
-define("DBLOGIN", "root");
-define("DBPWD", "root");
+define("DBNAME", "viroulaud8");
+define("DBLOGIN", "viroulaud8");
+define("DBPWD", "viroulaud8");
