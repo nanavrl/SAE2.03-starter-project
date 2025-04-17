9deb9312dfc58ff5e77c0e647b7011b6bc44831f
Viroulaud Anna <viroulaud8@mmi-dev.unilim.fr>
2025-04-17 23:46:26 +0200

    it-sur--css

--- IT-SURPRISE/app/component/movieCategory/script.js
+++ IT-SURPRISE/app/component/movieCategory/script.js
@@ -9,52 +9,15 @@ MovieCategory.format = function (category) {
     let categoryHtml = template;
     categoryHtml = categoryHtml.replace("{{categoryName}}", category.name);
 
+    const uniqueId = `carousel-${category.name.replace(/\s+/g, "-").toLowerCase()}`;
+    categoryHtml = categoryHtml.replaceAll("{{id}}", uniqueId);
+
     let moviesListHtml = Movie.format(category.movies || []);
     categoryHtml = categoryHtml.replace("{{movieCard}}", moviesListHtml);
 
     return categoryHtml;
 };
 
-function setupCarousel(wrapperSelector, cardsPerPage = 4) {
-    const wrapper = document.querySelector(wrapperSelector);
-    if (!wrapper) return;
-  
-    const track = wrapper.querySelector('.carousel-track .movies');
-    const cards = wrapper.querySelectorAll('.movie-card');
-    const prev = wrapper.querySelector('.carousel-btn.prev');
-    const next = wrapper.querySelector('.carousel-btn.next');
-  
-    let currentPage = 0;
-    const totalPages = Math.ceil(cards.length / cardsPerPage);
-  
-    // Définir largeur de chaque carte pour être en ligne
-    cards.forEach(card => {
-      card.style.flex = `0 0 calc(100% / ${cardsPerPage})`;
-    });
-  
-    // Affiche la bonne "page"
-    function updateCarousel() {
-      const translateX = -(100 / cardsPerPage) * currentPage;
-      track.style.transform = `translateX(${translateX}%)`;
-    }
-  
-    next.addEventListener('click', () => {
-      if (currentPage < totalPages - 1) {
-        currentPage++;
-        updateCarousel();
-      }
-    });
-  
-    prev.addEventListener('click', () => {
-      if (currentPage > 0) {
-        currentPage--;
-        updateCarousel();
-      }
-    });
-  
-    updateCarousel();
-  }
-  
 
 export { MovieCategory };
 
